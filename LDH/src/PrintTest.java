e4d0b2561ee47718def0a54633f4f0a95de6a5a0
EZEN-132 <EZEN-132@DESKTOP-0LN9KIJ>
2022-07-13 15:50:47 +0900

    AWS과정 학생 이니셜

---
+++ LDH/src/PrintTest.java
@@ -0,0 +1,17 @@
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		
+
+//		KJM KSJ KTH NKH PYM SDY LYB LJH JYS JHS HWJ CEJ
+		
+		System.out.println("OGW      KJS      ABK");
+		System.out.println("KJM      KSJ      KTH");
+		System.out.println("NKH      PYM      SDY");
+		System.out.println("LYB      LJH      JYS");
+		System.out.println("JHS      HWJ      CEJ");
+		
+		
+ 	}
+}
